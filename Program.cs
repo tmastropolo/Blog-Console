4f17599e0bc13b8d20e14b98de9cd7c7fb806abd
tmastropolo <tmastropolo@my.wctc.edu>
2019-12-11 11:09:16 -0600

    Menu

--- Program.cs
+++ Program.cs
@@ -13,26 +13,49 @@ namespace BlogsConsole
             logger.Info("Program started");
             try
             {
-
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                int choice = 0;
+                do
                 {
-                    Console.WriteLine(item.Name);
+
+
+                    Console.WriteLine("1) Display all Blogs");
+                    Console.WriteLine("2) Add Blog");
+                    Console.WriteLine("3) Create post");
+
+                    if (choice == 2)
+                    {
+                        // Create and save a new Blog
+                        Console.Write("Enter a name for a new Blog: ");
+                        var name = Console.ReadLine();
+
+                        var blog = new Blog { Name = name };
+
+                        var db = new BloggingContext();
+                        db.AddBlog(blog);
+                        logger.Info("Blog added - {name}", name);
+                    }
+
+                    else if (choice == 1)
+                    {
+                        var db = new BloggingContext();
+                        // Display all Blogs from the database
+                        var query = db.Blogs.OrderBy(b => b.Name);
+
+                        Console.WriteLine("All blogs in the database:");
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine(item.Name);
+                        }
+                    }
+
+                    else if (choice == 3)
+                    {
+                        var db = new BloggingContext();
+
+                        Console.WriteLine("Create post");
+                    }
+                } while (choice == 4);
                 }
-            }
             catch (Exception ex)
             {
                 logger.Error(ex.Message);
